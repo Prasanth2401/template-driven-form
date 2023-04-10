1bbe8b1066e7aceefcafa46aa770d7c923f01a87
Prasanth <prasanthraj.2401@gmail.com>
2023-04-10 18:39:28 +0530

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: ' app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'template-driven-form';
+  title = "template-driven-form";
+  form = {
+    fname:'',
+    lname:'',
+    email:'',
+    age:'',
+    number:''
+  }
+
+  integerRegex=(/^-?(0|[1-9]\d*)?$/);
+  emailRegex=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+  registerFn(){
+    console.log(this.form)
+  }
+  
 }
